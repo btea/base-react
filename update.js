cdc7a97f3358c446ae3e928dd0a1f41d2ed38b63
btea <2356281422@qq.com>
2021-10-11 15:49:18 +0800

    feat: 自动判断选择包管理工具更细所有安装包

--- update.js
+++ update.js
@@ -1,15 +1,50 @@
 const fs = require('fs');
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 
 const name = 'package.json';
+
+const alternativeCommands = {
+	npm: 'install ',
+	pnpm: 'install ',
+	yarn: 'add ',
+};
+
+function getPackageManger() {
+	const execPath = process.env.npm_execpath || 'npm';
+	if (execPath.includes('yarn')) {
+		return 'yarn';
+	}
+	if (execPath.includes('pnpm')) {
+		return 'pnpm';
+	}
+	if (execPath.includes('npm')) {
+		return 'npm';
+	}
+	return 'npm';
+}
 
 fs.readFile(name, (err, data) => {
 	if (err) {
 		process.exit(0);
 	}
 	const obj = JSON.parse(data.toString());
-	const list = [...Object.keys(obj.devDependencies || {}), ...Object.keys(obj.dependencies || {})];
+	const dep = Object.assign({}, obj.devDependencies, obj.dependencies);
+	const list = [...Object.keys(dep)];
 	if (list.length) {
-		exec(`npm install ${list.join(' ')}`);
+		const cmd = getPackageManger() + ' ' + alternativeCommands[getPackageManger()];
+		exec(
+			`${cmd}${list.join(' ')}`,
+			{
+				stdio: 'inherit',
+				shell: true,
+			},
+			(err, stdout) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(stdout);
+				}
+			}
+		);
 	}
 });
